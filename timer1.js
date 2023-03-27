cf8bee379e5002e03154705c526fcc4e3e1cd1f6
Hans Wang <hansmikaa@live.cn>
2023-03-27 02:35:48 -0400

    completed timer app

---
+++ timer1.js
@@ -0,0 +1,17 @@
+// get input from command line
+const args = process.argv.slice(2);
+
+for (let i = 0; i < args.length; i++) {
+  const seconds = Number(args[i]);
+
+  // verify it is number
+  if (isNaN(seconds)) {
+    process.stdout.write(`Invalid: ${args[i]}\n`);
+  } else if (seconds <= 0) {
+    process.stdout.write(`Skipping: ${seconds}\n`);
+  } else setTimeout(() => {
+    process.stdout.write(`Alarm at ${seconds} seconds!\n`);
+    // beep sound
+    process.stdout.write('\x07');
+  }, seconds * 1000);
+};
